70b503b4cef1572c7427532a8efd0dd48be94791
Kia <kiaaboalizadeh2@gmail.com>
2022-08-12 12:15:51 -0700

    fix everithing v1

--- lib/ui/home/bloc/home_bloc.dart
+++ lib/ui/home/bloc/home_bloc.dart
@@ -1,8 +1,7 @@
-import 'dart:math';
-
 import 'package:bloc/bloc.dart';
 import 'package:easy_task/data/repo/task_repo.dart';
 import 'package:easy_task/data/task.dart';
+import 'package:easy_task/utils.dart';
 import 'package:equatable/equatable.dart';
 import 'package:persian_datetime_picker/persian_datetime_picker.dart';
 
@@ -11,49 +10,58 @@ part 'home_state.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
   final ITaskRepository taskRepository;
+
   HomeBloc({required this.taskRepository}) : super(HomeLoading()) {
     on<HomeEvent>((event, emit) async {
       if (event is HomeStarted || event is SearchFieldClicked) {
         emit(HomeLoading());
         final String searchKey;
-        final String date;
 
         try {
+          List<TaskEntity> tasks = taskRepository.getTasks();
           if (event is SearchFieldClicked) {
             searchKey = event.searchKey;
-            date = event.date;
+            tasks = tasks
+                .where((element) =>
+                    element.title.contains(searchKey) ||
+                    element.tag.contains(searchKey))
+                .toList();
           } else if (event is HomeStarted) {
             searchKey = '';
-            date = event.date;
           } else {
-            date = Jalali.now().formatFullDate();
             searchKey = '';
           }
-          final tasks = taskRepository.getTasks();
+          //get all tasks from database
+
+          //get tag list if it's already empty
+          List<String> tags = [];
+          getTags(tags, tasks);
+          //get project list if it's already empty
+          List<String> projects = [];
+          getProjects(projects, tasks);
           final List<TaskEntity> tasksSearch;
-          if (searchKey.isNotEmpty) {
-            tasksSearch = tasks
-                .where((element) => element.title.contains(searchKey))
-                .toList();
-          } else {
-            tasksSearch = tasks;
-          }
+
           if (tasks.isEmpty && event is HomeStarted) {
             emit(EmptyState());
           } else {
-            emit(HomeSuccess(tasks, tasksSearch));
+            emit(HomeSuccess(tasks, projects, tags));
           }
         } catch (e) {
           emit(HomeError());
         }
-      } else if (event is UpdateTask) {
+      } else if (event is AddOrUpdateTask) {
+        //update or add task in database
         taskRepository.addOrUpdate(event.task);
       } else if (event is DeleteTask) {
-        for (var e in event.task) {
-          taskRepository.delete(e);
-        }
+        //remove task from database
+        taskRepository.delete(event.task);
       } else if (event is DeleteAll) {
+        //clear database
         taskRepository.deleteAll();
+      } else if (event is DeleteAllProjectTasks) {
+        for (var e in event.tasks) {
+          await taskRepository.delete(e);
+        }
       }
     });
   }
